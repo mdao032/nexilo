5ed76ea980c10df1059779e0fb5ed4156359ea1b
Dao <m.dao@outlook.fr>
2026-03-22 19:30:59 +0100

    init project

---
+++ src/main/java/com/nexilo/NexiloApplication.java
@@ -0,0 +1,25 @@
+package com.nexilo;
+
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class NexiloApplication {
+
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(NexiloApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(NexiloApplication.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        Environment env = app.run(args).getEnvironment();
+        logger.info("\n---------------------------\n\t" +
+                "Application '{}' is running! Access URLs: Local: http://localhost:{}" +
+                "\n---------------------------\n",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port")
+                );
+    }
+}
